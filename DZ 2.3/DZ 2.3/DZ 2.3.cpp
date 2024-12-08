72bccb81e670522565cf17a00571f2ecd20a734b
Molbert <askoysevski@gmail.com>
2024-12-09 01:41:50 +0300

    DZ 2.3

---
+++ DZ 2.3/DZ 2.3/DZ 2.3.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Adres
+{
+    string city;
+    string street;
+    int houseNumber;
+    int apartmentNumber;
+    int index;
+
+};
+
+void CoutAdres(Adres adr)
+{
+    cout << "Город "<< adr.city;
+    cout << endl;
+    cout << "Улица " << adr.street;
+    cout << endl;
+    cout << "Номер дома " << adr.houseNumber;
+    cout << endl;
+    cout << "Номер Квартиры " << adr.apartmentNumber;
+    cout << endl;
+    cout << "Индекс " << adr.index;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    Adres Mos{"Москва","Арбат",12,8,123456};
+    Adres Ize{ "Ижевск","Пушкина",59,143,953769 };
+
+    CoutAdres(Mos);
+    cout << endl << endl;
+    CoutAdres(Ize);
+}
+
