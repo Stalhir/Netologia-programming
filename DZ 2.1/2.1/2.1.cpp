d39ece76487482d2e6ad76668a54c19f0d3ca710
Molbert <askoysevski@gmail.com>
2024-12-09 01:24:32 +0300

    DZ 2.1 and DZ 2.2

---
+++ DZ 2.1/2.1/2.1.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+
+using namespace std;
+
+enum months
+{
+    january = 1,
+    february,
+    march,
+    april,
+    may,
+    jun,
+    july, 
+    august, 
+    september, 
+    october, 
+    november, 
+    december
+};
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    
+    int temp=1;
+    while (temp != 0) {
+        cout << "Введите номер месяца: ";
+        cin >> temp;
+        switch (temp)
+        {
+        case 0:
+            cout << "До свидания";
+            return 0;
+            break;
+        case january:
+            cout << "январь" << endl;
+            break;
+        case february:
+            cout << "февраль" << endl;
+            break;
+        case march:
+            cout << "март" << endl;
+            break;
+        case april:
+            cout << "апрель" << endl;
+            break;
+        case may:
+            cout << "май" << endl;
+            break;
+        case jun:
+            cout << "июнь" << endl;
+            break;
+        case july:
+            cout << "июль" << endl;
+            break;
+        case august:
+            cout << "август" << endl;
+            break;
+        case september:
+            cout << "сентябрь" << endl;
+            break;
+        case october:
+            cout << "октябрь" << endl;
+            break;
+        case november:
+            cout << "ноябрь" << endl;
+            break;
+        case december:
+            cout << "декабрь" << endl;
+            break;
+        default:
+            cout << "Неправильный номер!" << endl;
+            break;
+        }
+    }
+}
+
---
+++ DZ 2.2/DZ 2.2/DZ 2.2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+struct BankAccount
+{
+	string name; 
+	int balanceNumber;  
+	float balance;
+
+};
+
+void setBalance(BankAccount* b)
+{
+	cout << "Введите новый баланс: ";
+	cin >> b->balance;
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	 
+
+	setlocale(LC_ALL, "rus");
+	BankAccount b;
+	cout << "Введите номер счёта: ";
+	cin >> b.balanceNumber; 
+	cout << "Введите имя владельца: ";
+	cin >> b.name;
+	cout << "Введите баланс: ";
+	cin >> b.balance;
+	setBalance(&b);
+	cout << "Ваш счёт: " << b.name << ", " << b.balanceNumber << ", " << b.balance;
+}
+
