4a6de491aa0014085ce38525c5bb6bc0c928e8ce
Molbert <askoysevski@gmail.com>
2024-12-16 02:25:18 +0300

    DZ 3.1 3.2

---
+++ DZ 3.1/DZ 3.1/DZ 3.1.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Calculator
+{ 
+public:
+	double num1;
+	double num2;
+	double add() { return num1 + num2; };
+	double multiply() { return num1 * num2; };
+	double subtract_1_2() { return num1 - num2; };
+	double subtract_2_1() { return num2 - num1; };
+	double divide_1_2() { return num1 / num2; };
+	double divide_2_1() { return num2 / num1; };
+	bool set_num1(double num1) 
+	{
+		if (num1 != 0)
+		{
+			this->num1 = num1;
+			return true;
+		}
+		else
+		{
+			cout << "Неверное ввод" << endl;
+			return false;
+		}
+	};
+	bool set_num2(double num2) 
+	{
+		if (num2 != 0)
+		{
+			this->num2 = num2;
+			return true;
+		}
+		else
+		{
+			cout << "Неверное ввод" << endl;
+			return false;
+		}
+	};
+};
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	double a{}, b{};
+
+	Calculator A;
+	
+	while(true)
+	{
+		cout << "Введите num1: ";
+		cin >> a;
+		cout << "Введите num2: ";
+		cin >> b;
+		A.set_num1(a);
+		A.set_num2(b); 
+		cout << "num1 + num2 = " << A.add() << endl;
+		cout << "num1 - num2 = " << A.subtract_1_2() << endl;
+		cout << "num2 - num1 = " << A.subtract_2_1() << endl;
+		cout << "num1 * num2 = " << A.multiply() << endl; 
+		cout << "num1 / num2 = " << A.divide_1_2() << endl;
+		cout << "num2 / num1 = " << A.divide_2_1() << endl;
+	}
+
+}
---
+++ DZ 3.2/DZ 3.2/DZ 3.2.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+class Counter
+{
+private:
+	int count{1}; 
+public:
+	int getcount() const { return count; };
+	void setcount(int count) { this->count = count; };
+
+	void AddCount() { ++count; };
+	void MinusCount() { --count; };
+
+	Counter(int count = 1) { this->count = count; };
+};
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int count{};
+	
+	cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+	string YorN;
+	cin >> YorN;
+	
+	if(YorN == "да")
+	{
+		cout << "Введите начальное значение счётчика: ";
+		cin >> count;
+		
+	}
+	else {}
+
+	Counter counter;
+	
+	
+	while(true)
+	{
+		cout << "Введите команду ('+', '-', '=' или 'x'): ";
+		cin >> YorN;
+		
+		if (YorN == "+") { counter.AddCount(); }
+		else if (YorN == "-") { counter.MinusCount(); }
+		else if (YorN == "=") { cout << counter.getcount() << endl; }
+		else if (YorN == "x") { cout << "До свидания!" << endl; return 0; }
+		else { cout << "неизвестная команда" << endl; }
+			
+	}
+	
+
+
+
+}
+
