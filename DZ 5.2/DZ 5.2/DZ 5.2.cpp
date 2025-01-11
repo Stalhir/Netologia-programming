eeb3aae8d4c9fd2decc75b261352103b61b2de43
Molbert <askoysevski@gmail.com>
2025-01-11 16:45:52 +0300

    fix DZ 5.2

--- DZ 5.2/DZ 5.2/DZ 5.2.cpp
+++ DZ 5.2/DZ 5.2/DZ 5.2.cpp
@@ -9,17 +9,17 @@ class Figure
 protected:
     int sides_count{ 0 };
     Figure(int sides_count) { this->sides_count = sides_count; };
+    bool blocked = false;
+    //если обьект не подходит по параметру то функция с обьектом не будет работать
 public:
     int get_sides_count()
     {
         return sides_count;
     }
-    virtual string get_name()
-    {
-        return "Фигура";
-    }
+  
     virtual void print_info()
     {
+        if (blocked == true) { return; }
         cout << sides_count << endl;
     }
     Figure() { sides_count = 0; }
@@ -35,10 +35,7 @@ protected:
     int A, B, C; 
 
 public:
-    string get_name() override
-    {
-        return "Треугольник";
-    }
+    
     Triangle(int a, int b, int c, int A, int B, int C) : Figure(3)
     {
         this->a = a; this->b = b; this->c = c;
@@ -46,6 +43,7 @@ public:
     };
     void print_info() override 
     {
+        if (blocked == true) { return; }
         cout << "Треугольник: " << endl;
         cout << "Cтороны: a=" << a << " b=" << b << " c=" << c << endl;
         cout << "Углы: A=" << A << " B=" << B << " C=" << C << endl;
@@ -59,10 +57,11 @@ public:
     RAtriangle(int a, int b, int c, int A, int B, int C) : Triangle(a, b, c, A, B, C)
     {
         if(C == 90){}
-        else {  cout << "Error1"; delete this; }
+        else { blocked = true; }
     };
     void print_info() override
     {
+        if (blocked == true) { return; }
         cout << "Прямоугольный треугольник: " << endl;
         cout << "Cтороны: a=" << a << " b=" << b << " c=" << c << endl;
         cout << "Углы: A=" << A << " B=" << B << " C=" << C << endl;
@@ -76,10 +75,11 @@ public:
     Itriangle(int a, int b, int c, int A, int B, int C) : Triangle(a, b, c, A, B, C)
     {
         if (a == c && A == C) {}
-        else {  cout << "Error2"; delete this;}
+        else {   blocked = true;}
     };
     void print_info() override
     {
+        if (blocked == true) { return; }
         cout << "Равнобедренный треугольник: " << endl;
         cout << "Cтороны: a=" << a << " b=" << b << " c=" << c << endl;
         cout << "Углы: A=" << A << " B=" << B << " C=" << C << endl;
@@ -92,10 +92,11 @@ public:
     Etriangle(int a, int b, int c, int A, int B, int C) : Triangle(a, b, c, A, B, C)
     {
         if (a == b && c == b && a == c && A == 60 && B == 60 && C == 60) {}
-        else {  cout << "Error3"; delete this;}
+        else {  blocked = true;}
     };
     void print_info() override
     {
+        if (blocked == true) { return; }
         cout << "Равносторонний треугольник: " << endl;
         cout << "Cтороны: a=" << a << " b=" << b << " c=" << c << endl;
         cout << "Углы: A=" << A << " B=" << B << " C=" << C << endl;
@@ -110,10 +111,7 @@ protected:
     
     int A, B, C, D;
 public:
-    string get_name() override
-    {
-        return "Четырёхугольник";
-    }
+   
     Quadrangle(int a, int b, int c, int d, int A, int B, int C, int D) : Figure(4)
     {
         this->a = a; this->b = b; this->c = c; this->d = d; 
@@ -122,6 +120,7 @@ public:
     };
     void print_info() override
     {
+        if (blocked == true) { return; }
         cout << "Четырёхугольник: " << endl;
         cout << "Cтороны: a=" << a << " b=" << b << " c=" << c << " d="<<d << endl;
         cout << "Углы: A=" << A << " B=" << B << " C=" << C << " D=" << D<< endl;
@@ -134,10 +133,11 @@ public:
     Rectangle(int a, int b, int c, int d, int A, int B, int C, int D) : Quadrangle(a, b, c, d, A, B, C, D)
     {
         if(a == c && b == d && A == 90 && B == 90 && C == 90 && D == 90){}
-        else { cout << "Error4"; delete this;}
+        else {  blocked = true;}
     };
     void print_info() override
     {
+        if (blocked == true) { return; }
         cout << "Прямоугольник: " << endl;
         cout << "Cтороны: a=" << a << " b=" << b << " c=" << c << " d=" << d << endl;
         cout << "Углы: A=" << A << " B=" << B << " C=" << C << " D=" << D << endl;
@@ -150,10 +150,11 @@ public:
     Square(int a, int b, int c, int d, int A, int B, int C, int D) : Quadrangle(a, b, c, d, A, B, C, D)
     {
         if (a == c && a == b && a == d && b == d && c == d && A == 90 && B == 90 && C == 90 && D == 90) {} 
-        else { cout << "Error5"; delete this;}
+        else {  blocked = true;}
     };
     void print_info() override
     {
+        if (blocked == true) { return; }
         cout << "Квадрат: " << endl;
         cout << "Cтороны: a=" << a << " b=" << b << " c=" << c << " d=" << d << endl;
         cout << "Углы: A=" << A << " B=" << B << " C=" << C << " D=" << D << endl;
@@ -166,10 +167,11 @@ public:
     Parallelogram(int a, int b, int c, int d, int A, int B, int C, int D) : Quadrangle(a, b, c, d, A, B, C, D)
     {
         if (a == c && b == d && A == C && B == D) {}
-        else { cout << "Error6"; delete this; }
+        else {  blocked = true; }
     };
     void print_info() override
     {
+        if (blocked == true) { return; }
         cout << "Параллелограмм: " << endl;
         cout << "Cтороны: a=" << a << " b=" << b << " c=" << c << " d=" << d << endl;
         cout << "Углы: A=" << A << " B=" << B << " C=" << C << " D=" << D << endl;
@@ -182,11 +184,13 @@ public:
     Rhomb(int a, int b, int c, int d, int A, int B, int C, int D) : Quadrangle(a, b, c, d, A, B, C, D)
     {
         if (a == c&& a== b && a == d && b == d && c == d && A == C && B == D) {} 
-        else { cout << "Error7"; delete this;}
+        else { blocked = true;}
     
     };
     void print_info() override
     {
+        if (blocked == true) { return; }
+        
         cout << "Ромб: " << endl;
         cout << "Cтороны: a=" << a << " b=" << b << " c=" << c << " d=" << d << endl;
         cout << "Углы: A=" << A << " B=" << B << " C=" << C << " D=" << D << endl;
@@ -200,7 +204,7 @@ int main()
     Triangle T(10, 20, 30, 50, 60, 70); 
     RAtriangle RAT(10,20,30,50,60,90); 
     Itriangle  IT(10,20,10,50,60,50);  
-    Etriangle ET(30,30,30,60,60,60); 
+    Etriangle ET(300,30,30,60,60,60); 
   
     Quadrangle Q(10, 20, 30, 40, 50, 60, 70, 80); 
     Rectangle R(10, 20, 10, 20, 90, 90, 90, 90); 
