98ed032bfc4203b6e4322cf01be2763997ebf0cd
Molbert <askoysevski@gmail.com>
2025-01-11 16:47:44 +0300

    Fix DZ 5.2 two

--- DZ 5.2/DZ 5.2/DZ 5.2.cpp
+++ DZ 5.2/DZ 5.2/DZ 5.2.cpp
@@ -204,7 +204,7 @@ int main()
     Triangle T(10, 20, 30, 50, 60, 70); 
     RAtriangle RAT(10,20,30,50,60,90); 
     Itriangle  IT(10,20,10,50,60,50);  
-    Etriangle ET(300,30,30,60,60,60); 
+    Etriangle ET(30,30,30,60,60,60); 
   
     Quadrangle Q(10, 20, 30, 40, 50, 60, 70, 80); 
     Rectangle R(10, 20, 10, 20, 90, 90, 90, 90); 
