a86f764593df010513b0dda61163a64fd102bb58
Molbert <askoysevski@gmail.com>
2025-02-22 01:43:42 +0300

    Add DZ 11.3

---
+++ DZ 11.3/DZ11.3/DZ11.2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "Windows.h" 
+#include "Leaver.h"
+
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "rus");
+    std::string temp;
+    Leaver a;
+    std::cout << "Введите имя:";
+    std::cin >> temp;
+    std::cout << a.leave(temp) << std::endl;
+    std::cin >> temp;
+}
