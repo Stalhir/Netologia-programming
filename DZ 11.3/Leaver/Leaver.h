c151a412ffb196c37fa0f2d944605d93563592f1
Molbert <askoysevski@gmail.com>
2025-03-02 22:52:24 +0300

    fix DZ 11.3

--- DZ 11.3/Leaver/Leaver.h
+++ DZ 11.3/Leaver/Leaver.h
@@ -1,13 +1,15 @@
 #include <string>
 #pragma once
+/*
 #ifdef LEAVER_EXPORTS
 #define LEAVERLIBRARY_API __declspec(dllexport)
 #else
 #define LEAVERLIBRARY_API __declspec(dllimport)
 #endif
+*/
 
 	class Leaver
 	{
 	public:
-		LEAVERLIBRARY_API std::string leave(std::string a);
+	std::string leave(std::string a);
 	};
