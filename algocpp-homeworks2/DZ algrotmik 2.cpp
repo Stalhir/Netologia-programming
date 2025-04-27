8df3dd4473189502d4f703e3c17889aeb1503b37
Molbert <askoysevski@gmail.com>
2025-04-27 23:27:38 +0300

    Add DZ algr 2

---
+++ algocpp-homeworks2/DZ algrotmik 2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2); 
+}
+
+int main() {
+    setlocale(LC_ALL, "rus"); 
+    int n;
+    std::cout << "Введите номер числа Фибоначчи: ";
+    std::cin >> n;
+
+    std::cout << "Число Фибоначчи в позиции " << n << " равно " << fibonacci(n) << std::endl;
+    return 0;
+}
