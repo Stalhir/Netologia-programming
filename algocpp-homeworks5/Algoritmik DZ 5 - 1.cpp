5c36392cdd5fb8eabfa827b559b50082bf3186c9
Molbert <askoysevski@gmail.com>
2025-05-22 00:41:23 +0300

    ADD 5 algocpp homework5

---
+++ algocpp-homeworks5/Algoritmik DZ 5 - 1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+
+void print_element(int level, const char* relation, int idx, int arr[]) {
+    if (level == 0) {
+        std::cout << level << " " << relation << " " << arr[idx] << std::endl;
+    }
+    else {
+        std::cout << level << " " << relation << "(" << arr[(idx - 1) / 2] << ") " << arr[idx] << std::endl;
+    }
+}
+
+void print_pyramid(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        int level = 0;
+        int current = i;
+
+        
+        while (current > 0) {
+            current = (current - 1) / 2;
+            level++;
+        }
+
+        
+        if (i == 0) {
+            print_element(level, "root", i, arr);
+        }
+        else if (i % 2 == 1) {
+            print_element(level, "left", i, arr);
+        }
+        else {
+            print_element(level, "right", i, arr);
+        }
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    // Тестовые массивы внизу
+    int arr1[] = { 1, 3, 6, 5, 9, 8 };
+    int arr2[] = { 94, 67, 18, 44, 55, 12, 6, 42 };
+    int arr3[] = { 16, 11, 9, 10, 5, 6, 8, 1, 2, 4 };
+
+    std::cout << "Исходный массив: 1 3 6 5 9 8" << std::endl;
+    std::cout << "Пирамида:" << std::endl;
+    print_pyramid(arr1, sizeof(arr1) / sizeof(arr1[0]));
+
+    std::cout << "Исходный массив: 94 67 18 44 55 12 6 42" << std::endl;
+    std::cout << "Пирамида:" << std::endl;
+    print_pyramid(arr2, sizeof(arr2) / sizeof(arr2[0]));
+
+    std::cout << "Исходный массив: 16 11 9 10 5 6 8 1 2 4" << std::endl;
+    std::cout << "Пирамида:" << std::endl;
+    print_pyramid(arr3, sizeof(arr3) / sizeof(arr3[0]));
+
+    return 0;
+}
---
+++ algocpp-homeworks5/Algoritmik DZ 5 - 2.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+void printPyramid(int arr[], int size) {
+    int level = 0;
+    int index = 0;
+
+    while (index < size) {
+        cout << level << " ";
+        for (int i = 0; i < pow(2, level) && index < size; i++) {
+            if (i == 0) {
+                cout << "root " << arr[index];
+            }
+            else {
+                cout << "left(" << arr[(index - 1) / 2] << ") " << arr[index];
+            }
+            index++;
+            if (i % 2 == 1) {
+                cout << " ";
+            }
+        }
+        cout << endl;
+        level++;
+    }
+}
+
+int getParentIndex(int index) {
+    return (index - 1) / 2;
+}
+
+int getLeftChildIndex(int index) {
+    return 2 * index + 1;
+}
+
+int getRightChildIndex(int index) {
+    return 2 * index + 2;
+}
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    
+    int pyramid[] = { 1, 3, 6, 5, 9, 8 };
+    int size = sizeof(pyramid) / sizeof(pyramid[0]);
+
+    cout << "Исходный массив: ";
+    for (int i = 0; i < size; i++) {
+        cout << pyramid[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Пирамида:" << endl;
+    printPyramid(pyramid, size);
+
+    int currentIndex = 0;
+    string command;
+
+    while (true) {
+        cout << "Вы находитесь здесь: " << currentIndex << " ";
+        if (currentIndex == 0) {
+            cout << "root " << pyramid[currentIndex] << endl;
+        }
+        else {
+            cout << "left(" << pyramid[getParentIndex(currentIndex)] << ") " << pyramid[currentIndex] << endl;
+        }
+
+        cout << "Введите команду: ";
+        cin >> command;
+
+        if (command == "up") {
+            if (currentIndex == 0) {
+                cout << "Ошибка! Отсутствует родитель" << endl;
+            }
+            else {
+                currentIndex = getParentIndex(currentIndex);
+                cout << "Ок" << endl;
+            }
+        }
+        else if (command == "left") {
+            int leftChildIndex = getLeftChildIndex(currentIndex);
+            if (leftChildIndex < size) {
+                currentIndex = leftChildIndex;
+                cout << "Ок" << endl;
+            }
+            else {
+                cout << "Ошибка! Отсутствует левый потомок" << endl;
+            }
+        }
+        else if (command == "right") {
+            int rightChildIndex = getRightChildIndex(currentIndex);
+            if (rightChildIndex < size) {
+                currentIndex = rightChildIndex;
+                cout << "Ок" << endl;
+            }
+            else {
+                cout << "Ошибка! Отсутствует правый потомок" << endl;
+            }
+        }
+        else if (command == "exit") {
+            break;
+        }
+        else {
+            cout << "Ошибка! Неверная команда" << endl;
+        }
+    }
+
+    return 0;
+}
