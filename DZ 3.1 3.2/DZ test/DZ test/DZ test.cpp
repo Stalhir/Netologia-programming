0659814e8fc62b7e06059abdb307df750fd3c31d
Molbert <askoysevski@gmail.com>
2025-06-18 00:01:39 +0300

    DZ 3.1 3.2

---
+++ DZ 3.1 3.2/DZ test/DZ test/DZ test.cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+
+
+
+
+class smart_array
+{
+private:
+    int arraySize{};
+    int array_addIndex{};
+    int* smartArray;
+public:
+
+    smart_array(int _arraySize) : arraySize(_arraySize)
+    { 
+        smartArray = new int[_arraySize];
+    }
+
+    smart_array(const smart_array& other) :
+        arraySize(other.arraySize),
+        array_addIndex(other.array_addIndex),
+        smartArray(new int[other.arraySize])
+    {
+        std::copy(other.smartArray, other.smartArray + arraySize, smartArray); 
+    }
+
+    smart_array& operator=(smart_array other)
+    {
+
+        for (int i = 0; i < this->arraySize; i++)
+        {
+            this->smartArray[i] = other.smartArray[i];
+                
+        }
+        return *this;
+    }
+
+
+    void add_element(int element)
+    {
+        if(array_addIndex+1>arraySize)
+        {
+            throw std::out_of_range("Index out of range");
+        }
+        smartArray[array_addIndex] = element;
+        array_addIndex++;
+    }
+
+    int get_element(int index)
+    {
+        if(index >= arraySize)
+        {
+            throw std::out_of_range("Index out of range");
+        }
+        return smartArray[index];
+    }
+
+    ~smart_array()
+    {
+        delete[] smartArray;
+    }
+};
+
+
+
+
+
+
+int main()
+{
+    try {
+        smart_array arr(5);
+        arr.add_element(1);
+        arr.add_element(4);
+        arr.add_element(155);
+        arr.add_element(14);
+        arr.add_element(15);
+       
+        std::cout << arr.get_element(1) << std::endl;
+
+
+        smart_array new_arr(6);
+        new_arr.add_element(231);
+        new_arr.add_element(334); 
+        new_arr.add_element(155); 
+        new_arr.add_element(1422); 
+        new_arr.add_element(135); 
+        new_arr.add_element(1315); 
+        arr = new_arr;
+
+        std::cout << arr.get_element(3) << std::endl;
+    }
+    catch (const std::exception& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+}
+
