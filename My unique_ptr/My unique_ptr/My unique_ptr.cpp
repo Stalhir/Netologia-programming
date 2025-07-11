916f7a8dbac3f50e5d33e96531c012d531aeb9c7
Arsen <askoysevski@gmail.com>
2025-07-11 22:14:57 +0300

    add DZ files smart pointer

---
+++ My unique_ptr/My unique_ptr/My unique_ptr.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+
+
+template<typename T>
+class MyUnique_ptr
+{
+private:
+    T* badPtr = nullptr;
+
+
+public:
+    explicit MyUnique_ptr(T* pointer) : badPtr(pointer)
+    {
+
+    }
+    ~MyUnique_ptr()
+    {
+        delete badPtr;
+    }
+
+    MyUnique_ptr(const MyUnique_ptr&) = delete;
+    MyUnique_ptr& operator=(const MyUnique_ptr&) = delete;
+     
+    T* release()
+    {
+        delete badPtr;
+        badPtr = nullptr;
+        return badPtr;
+    }
+    
+
+    T& operator*()
+    {
+        return *badPtr;
+    }
+
+    T* operator->()
+    {
+        return badPtr;
+    }
+
+};
+
+
+
+
+
+int main()
+{
+
+    int a{10};
+    MyUnique_ptr<int> natural(new int);
+  
+
+}
