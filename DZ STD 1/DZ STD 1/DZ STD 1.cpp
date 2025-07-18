2f625856b94cf7231adaa23689a199a4c6b95475
Stalhir <askoysevski@gmail.com>
2025-07-18 12:13:44 +0300

    Add DZ STD 1 and 2

---
+++ DZ STD 1/DZ STD 1/DZ STD 1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	string text{ "Hello world!!" };
+	std::unordered_map<char, int> freq;
+
+	for (auto c : text) {
+		freq[c]++;
+	}
+
+	std::vector<std::pair<char, int>> sorted(freq.begin(), freq.end());
+
+	for (int i = 0; i < sorted.size(); i++)
+	{
+		for (int j = 0; j < sorted.size() - i - 1; j++)
+		{
+			if (sorted[j].second < sorted[j+1].second)
+			{
+				swap(sorted[j], sorted[j + 1]);
+			}
+		}
+	}
+	
+
+
+	for (auto a : sorted) {
+		std::cout << a.first << ": " << a.second << std::endl;
+	}
+}
---
+++ DZ STD 2/DZ STD 2/DZ STD 2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <set>
+
+
+using namespace std;
+
+int main()
+{
+	int Size, in;
+
+	cout << "Size ";
+	cin >> Size;
+	set<int, greater<>> uniqSortArr;
+	for (int i = 0; i < Size; i++)
+	{
+		cin >> in;
+		uniqSortArr.insert(in);
+		in = 0;
+	}
+	for(auto el : uniqSortArr)
+	{
+		cout << el << endl;
+	}
+	
+
+}
