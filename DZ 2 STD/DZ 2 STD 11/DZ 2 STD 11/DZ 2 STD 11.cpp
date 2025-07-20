589d763693e8dc572e678344ba3a038b1e1d1a56
Stalhir <askoysevski@gmail.com>
2025-07-20 16:36:36 +0300

    Add std 2 Dz

---
+++ DZ 2 STD/DZ 2 STD 11/DZ 2 STD 11/DZ 2 STD 11.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template<typename T>
+void noDuplicate(vector<T>& vec)
+{
+	std::sort(vec.begin(), vec.end());
+	auto it = std::unique(vec.begin(), vec.end());
+	vec.erase(it, vec.end());
+}
+
+int main()
+{
+	vector<int> a{ 1, 1, 2, 5, 6, 1, 2, 4 };
+	noDuplicate(a);
+	for(int i = 0; i < a.size(); i++)
+	{
+		cout << a[i] << endl;
+	}
+}
---
+++ DZ 2 STD/DZ 2 STD 2/DZ 2 STD 2/DZ 2 STD 2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+template<typename T>
+void print_container(const T& cont)
+{
+    for (auto it = cont.begin(); it != cont.end(); ++it) {
+        cout << *it << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set); 
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list); 
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector);  
+}
