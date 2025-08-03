36b955026a50d1d0d13b308523fefb0ba816f603
Stalhir <askoysevski@gmail.com>
2025-08-03 16:41:04 +0300

    add DZ parall 2.1 and 2.2

---
+++ DZ paral 2.2/DZ paral 2.2/DZ paral 2.2.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <chrono>
+#include <Windows.h>
+#include <random>
+
+using namespace std;
+
+mutex cout_mutex;
+
+class consol_parameter
+{
+public:
+    static void SetColor(int text, int background)
+    {
+        SetConsoleTextAttribute(hStdOut, (WORD)((background << 4) | text));
+    }
+    static void SetPosition(int x, int y)
+    {
+        COORD point;
+        point.X = x;
+        point.Y = y;
+        SetConsoleCursorPosition(hStdOut, point);
+    }
+private:
+    static HANDLE hStdOut;
+};
+HANDLE consol_parameter::hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+void ProgresBar(int num, int steps)
+{
+    auto start = chrono::steady_clock::now();
+    
+    {
+        lock_guard<mutex> lock(cout_mutex);
+        consol_parameter::SetPosition(0, num + 2);
+        cout << num << " " << this_thread::get_id() << "\n";
+    }
+
+    srand(GetCurrentThreadId());
+    for (int i = 0; i < steps; i++)
+    {
+        {
+        lock_guard<mutex> lock(cout_mutex);
+        consol_parameter::SetPosition(i + 9, num + 2);
+        cout << static_cast<char>(219);
+        }
+        std::chrono::milliseconds sleep(rand()%1000);
+            
+        this_thread::sleep_for(sleep);
+    }
+
+    auto end = chrono::steady_clock::now();
+    {
+        lock_guard<mutex> lock(cout_mutex);
+        consol_parameter::SetPosition(steps+9, num + 2);
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        cout << elapsed_seconds.count() << "s";
+    }
+}
+
+void AllProgresBar(int countProgBar, int steps)// тут все прогресс бары типо выводятся и потоки создаются 
+{
+    cout << "#  ID          Progres Bar        Time" << endl;
+    vector<thread> Thrs;
+    for(int i = 0; i < countProgBar; i++)
+    {
+        Thrs.push_back(thread(ProgresBar, i, steps));
+    }
+    for (int i = 0; i < countProgBar; i++)
+    {
+        Thrs[i].join();
+    }
+
+    cout << "\033[" << countProgBar + 2 << ";0H" << endl;
+}
+
+
+int main()
+{
+    AllProgresBar(9, 18);
+     
+    //cout << "Natural"; 
+
+
+    // Короче будут строчки в каждой строчки прогресс бар и они выводятся одновременно. В видо лекции в конце показывает слоняра наш
+    // Это всё имитация и расчёт тоже
+}
+
---
+++ DZ parall 2.1/DZ parall 2.1/DZ parall 2.1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <thread>
+#include <atomic>
+#include <chrono>
+
+using namespace std;
+
+void Client(atomic<int>& client_count, int maxclient)
+{
+    for (int i = 0; i < maxclient; i++)
+    {
+        client_count.fetch_add(1, memory_order_seq_cst);
+        this_thread::sleep_for(1s);
+        //cout << "Client_Func: " << client_count.load(memory_order_seq_cst) << "\t";
+    }
+}
+
+void Operator(atomic<int>& client_count)
+{
+    while (client_count != 0)
+    {
+        client_count.fetch_sub(1, memory_order_seq_cst);
+        this_thread::sleep_for(2s);
+        //cout << "Operator_Func: " << client_count.load(memory_order_seq_cst) << "\t";
+    }
+}
+
+
+int main()
+{
+    atomic<int> client_count = 5;
+    thread t1(Client, ref(client_count), 8);
+    thread t2(Operator, ref(client_count));
+
+    t1.join();
+    t2.join();
+    return 0;
+}
