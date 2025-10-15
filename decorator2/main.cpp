9bfeec6249abf0db6cc0bd0498bac6d3dc63f1d9
Molbert <askoysevski@gmail.com>
2025-10-15 21:25:46 +0300

    Add decorator2

---
+++ decorator2/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "proxy.cpp"
+
+int main()
+{
+    // auto real_db = VeryHeavyDatabase();
+    // auto cached_db = CacheProxyDB(std::addressof(real_db));
+    // auto test_db = TestDB(std::addressof(real_db));
+    //
+    // std::cout << cached_db.GetData("key") << std::endl;
+    // std::cout << cached_db.GetData("key") << std::endl;
+    // std::cout << test_db.GetData("key") << std::endl;
+    auto real_db = VeryHeavyDatabase();
+    auto limit_db = OneShotDB(std::addressof(real_db), 2);
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+    return 0;
+}
